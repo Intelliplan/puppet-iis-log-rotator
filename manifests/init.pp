4a2ba86e44460360b6682127bf5ddb30e8f9cc4b
Kamil <khakim@kth.se>
2014-04-08 14:02:23 +0200

    Yet another typo..

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,8 +12,8 @@ class iis_log_rotator(
   scheduled_task { 'Remove Old IIS Logs':
     ensure      => present,
     enabled     => true,
-    command     => $remove_log_command,
-    arguments   => $::powershell_location,
+    command     => $::powershell_location,
+    arguments   => $remove_log_command,
     trigger     => $how_often_trigger,
     working_dir => $::iis_root_log_dir,
   }
